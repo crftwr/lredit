e3c9c9b6fafbd1c65cd7c9e97d549d684ddc447b
crftwr <craftware@gmail.com>
2017-12-28 19:30:31 -0800

    1行が長いファイルを生成するように

--- tool/create_large_file.py
+++ tool/create_large_file.py
@@ -1,10 +1,32 @@
+import json
 import random
 
-filesize = 1024 * 1024 * 1024
+filesize = 5 * 1024 * 1024 * 1024 # 5GB
 
 with open( "large.txt", "w" ) as fd:
+
+    i = 0
+    
     while True:
-        fd.write( "%d\n" % random.randint(0, 999999999999999999999999 ) )
+        
+        d = {}
+        d["index"] = i
+        d["data"] = []
+        for j in range(16):
+            d2 = {}
+            d2["a"] = random.random() * random.randint( 0, 10000 )
+            d2["b"] = random.random() * random.randint( 0, 10000 )
+            d2["c"] = random.random() * random.randint( 0, 10000 )
+            d2["d"] = random.random() * random.randint( 0, 10000 )
+            d["data"].append(d2)
+
+        s = json.dumps(d)
+
+        fd.write( s + "\n" )
         if fd.tell() >= filesize:
             break
-
+        
+        if i % 10000 == 0:
+            print( "num lines = %d, file size=%d\n", i, fd.tell() )
+        
+        i+=1
